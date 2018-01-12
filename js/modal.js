8545fdde996685d6517c4a5459fd69018a5a1cec
ivasenko <ivasenko.iren@gmail.com>
2018-01-12 02:15:05 +0200

    view

--- js/modal.js
+++ js/modal.js
@@ -2,14 +2,6 @@ var cart = document.getElementsByClassName('cart')[0];
 var cartModal = document.getElementsByClassName('cart__modal')[0];
 var btnClose = document.getElementsByClassName('del')[0];
 
-cart.onclick = function () {
-    cartModal.style.display = 'block';
-};
-btnClose.onclick = function () {
-    cartModal.style.display = 'none';
-};
-window.onclick = function(event) {
-    if (event.target === cartModal) {
-        cartModal.style.display = 'none';
-    }
-};
+cart.addEventListener('click', function(){
+    cartModal.style.display = (cartModal.style.display === 'none') ? 'block' : 'none';
+});
